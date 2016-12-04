a386c8a3135656f7c5dfb035703197dd7da5971e
Jeff Gold <jgold@esclat.net>
2016-12-03 23:58:37 -0500

    Move view along with character

--- whiplash.js
+++ whiplash.js
@@ -5,9 +5,24 @@
 (function(whiplash) {
     "use strict";
 
-    var player;
+    var state = {
+        height: 320, width: 320, zoom: 50,
+        characters: []
+    };
+    whiplash.changeZoom = function(value) {
+        state.zoom = value;
+    };
 
-    var drawVision = function(character, ctx, now) {
+    var drawBackground = function(ctx, state, now) {
+        ctx.beginPath();
+        ctx.moveTo(10, 0);
+        ctx.arc(10, 0, 1, 0, 2 * Math.PI);
+        ctx.fillStyle = 'green';
+        ctx.fill();
+    };
+
+    var drawVision = function(ctx, character, state, now) {
+        var size = character.size;
         ctx.save();
         ctx.translate(character.x, character.y);
         ctx.rotate(character.direction);
@@ -16,7 +31,7 @@
             character.visionColor) {
             ctx.beginPath();
             ctx.moveTo(0, 0);
-            ctx.arc(0, 0, character.size * character.visionRange,
+            ctx.arc(0, 0, size * character.visionRange,
                    -character.visionArc, character.visionArc);
             ctx.fillStyle = character.visionColor;
             ctx.fill();
@@ -24,22 +39,23 @@
         ctx.restore();
     }
 
-    var drawPerson = function(character, ctx, now) {
+    var drawPerson = function(ctx, character, state, now) {
+        var size = character.size;
         ctx.save();
         ctx.translate(character.x, character.y);
         ctx.rotate(character.direction);
 
         ctx.scale(0.8, 1);
         ctx.beginPath();
-        ctx.moveTo(character.size, 0);
-        ctx.arc(0, 0, character.size, 0, Math.PI * 2);
+        ctx.moveTo(size, 0);
+        ctx.arc(0, 0, size, 0, Math.PI * 2);
         ctx.fillStyle = character.bodyColor;
         ctx.fill();
 
         ctx.scale(1.25, 1);
         ctx.beginPath();
-        ctx.moveTo(character.size, 0);
-        ctx.arc(0, 0, character.size * 0.75, 0, Math.PI * 2);
+        ctx.moveTo(size, 0);
+        ctx.arc(0, 0, size * 0.75, 0, Math.PI * 2);
         ctx.fillStyle = character.headColor;
         ctx.fill();
 
@@ -47,14 +63,14 @@
             ((now + character.blinkPhase) % character.blinkFreq) >
             character.blinkLength) {
             ctx.beginPath();
-            ctx.arc(character.size * 0.2, character.size * -0.2,
-                    character.size * 0.1, 0, Math.PI * 2);
+            ctx.arc(size * 0.2, size * -0.2,
+                    size * 0.1, 0, Math.PI * 2);
             ctx.fillStyle = character.eyeColor;
             ctx.fill();
             ctx.beginPath();
-            ctx.moveTo(character.size, 0);
-            ctx.arc(character.size * 0.2, character.size * 0.2,
-                    character.size * 0.1, 0, Math.PI * 2);
+            ctx.moveTo(size, 0);
+            ctx.arc(size * 0.2, size * 0.2,
+                    size * 0.1, 0, Math.PI * 2);
             ctx.fillStyle = character.eyeColor;
             ctx.fill();
         }
@@ -64,14 +80,14 @@
     var makePlayer = function(x, y, size) {
         return {
             last: new Date().getTime(),
-            x: x, y: y, direction: 0, size: size,
+            x: x, y: y, direction: 0, size: size, speed: 0.009,
             aup: false, adown: false, aleft: false, aright: false,
             headColor: 'orangered',
             bodyColor: 'orange',
             eyeColor: 'blue',
             blinkFreq: 4000, blinkLength: 250, blinkPhase: 0,
             update: function(state, now) {
-                var steps = 0.25 * (now - this.last);
+                var steps = this.speed * (now - this.last);
                 var rots = 0.005 * (now - this.last);
                 if (this.aleft && !this.aright) {
                     this.direction -= rots;
@@ -88,33 +104,32 @@
                 }
                 this.last = now;
             },
-            drawPre: function(state, ctx, now) {
-                drawVision(this, ctx, now);
+            drawPre: function(ctx, state, now) {
+                drawVision(ctx, this, state, now);
             },
-            draw: function(state, ctx, now) {
-                this.size = state.width / 35;
-                drawPerson(this, ctx, now);
+            draw: function(ctx, state, now) {
+                drawPerson(ctx, this, state, now);
             }
         };
     };
 
-    var makeGuard = function(x, y, size) {
+   var makeGuard = function(x, y, size) {
         return {
             last: new Date().getTime(),
             headColor: 'blue',
             bodyColor: 'darkgray',
             eyeColor: 'black',
-            x: x, y: y, direction: 0, size: size,
+            x: x, y: y, direction: 0, size: size, speed: 0.008,
             blinkFreq: 1000, blinkLength: 100,
             blinkPhase: Math.random() * 1000,
-            visionRange: 5, visionArc: Math.PI / 3,
+            visionRange: 5, visionArc: Math.PI / 10,
             visionColor: 'rgba(255, 255, 255, 0.25)',
             update: function(state, now) {
-                var steps = 0.25 * (now - this.last);
+                var steps = this.speed * (now - this.last);
                 var rots = 0.005 * (now - this.last);
                 var vector = {
-                    x: player.x - this.x,
-                    y: player.y - this.y,
+                    x: state.player.x - this.x,
+                    y: state.player.y - this.y,
                 };
                 var length = Math.sqrt(
                     vector.x * vector.x + vector.y * vector.y);
@@ -122,7 +137,7 @@
                 vector.y /= length;
                 var dot = vector.x * Math.cos(this.direction) +
                           vector.y * Math.sin(this.direction);
-                if (dot < 0.95) {
+                if (dot < Math.cos(Math.PI / 10)) {
                     this.direction += rots;
                 } else if (length > this.size * this.visionRange) {
                     this.x += Math.cos(this.direction) * steps;
@@ -130,11 +145,11 @@
                 }
                 this.last = now;
             },
-            drawPre: function(state, ctx, now) {
-                drawVision(this, ctx, now);
+            drawPre: function(ctx, state, now) {
+                drawVision(ctx, this, state, now);
             },
-            draw: function(state, ctx, now) {
-                drawPerson(this, ctx, now);
+            draw: function(ctx, state, now) {
+                drawPerson(ctx, this, state, now);
             }
         };
     };
@@ -147,10 +162,6 @@
             display: 'block',
             color: '#222', background: '#ddd'
         }).appendTo(container);
-        var state = {
-           height: 320, width: 320,
-           characters: []
-        };
 
         var draw_id = 0, draw_last = 0;
         var draw = function() {
@@ -176,22 +187,37 @@
                 ctx.lineWidth = lineWidth;
                 ctx.clearRect(0, 0, width, height);
 
+                ctx.scale(state.zoom, state.zoom)
+                ctx.translate((width / (2 * state.zoom)) -
+                              state.player.x,
+                              (height / (2 * state.zoom)) -
+                              state.player.y);
+
+                drawBackground(ctx, state, now);
+
                 state.characters.forEach(function(character) {
                     if (character.drawPre)
-                        character.drawPre(state, ctx, now);
+                        character.drawPre(ctx, state, now);
                 });
+
                 state.characters.forEach(function(character) {
                     if (character.draw)
-                        character.draw(state, ctx, now);
+                        character.draw(ctx, state, now);
                 });
-                
-                ctx.restore();
 
+                state.characters.forEach(function(character) {
+                    if (character.drawPost)
+                        character.drawPost(ctx, state, now);
+                });
+
+                ctx.restore();
                 redraw();
             }
         };
+
         var redraw = function()
         { if (!draw_id) draw_id = requestAnimationFrame(draw); };
+
         var resize = function(event) {
 	    board.width(viewport.width());
 	    board.height(viewport.height() * 0.85);
@@ -208,51 +234,50 @@
 
             redraw();
         };
+
         board.on('click', function(event) {
             var row = Math.floor(event.offsetY * 3 / board.height());
             var col = Math.floor(event.offsetX * 3 / board.width());
         });
+
         board.resize(resize);
         resize();
 
-	player = makePlayer(
-            state.width / 2, state.height / 2, state.width / 35);
-        state.characters.push(makeGuard(
-            state.width / 5, state.height / 5, state.width / 35));
-        state.characters.push(makeGuard(
-            4 * state.width / 5, 4 * state.height / 5,
-            state.width / 35));
-	state.characters.push(player);
+        state.characters.push(makeGuard(-5, -5, 1));
+        state.characters.push(makeGuard(5, -5, 1));
+        state.characters.push(makeGuard(-5, 5, 1));
+        state.characters.push(makeGuard(5, 5, 1));
+	state.characters.push(state.player = makePlayer(0, 0, 1));
 
 	viewport.on('keydown', function(event) {
 	    if (event.keyCode == 37 || event.keyCode == 65) {
-		player.aleft = true;
+		state.player.aleft = true;
 	    } else if (event.keyCode == 38 || event.keyCode == 87) {
-                player.aup = true;
+                state.player.aup = true;
 	    } else if (event.keyCode == 39 || event.keyCode == 68) {
-		player.aright = true;
+		state.player.aright = true;
 	    } else if (event.keyCode == 40 || event.keyCode == 83) {
-		player.adown = true;
+		state.player.adown = true;
 	    }
 	});
+
 	viewport.on('keyup', function(event) {
 	    if (event.keyCode == 37 || event.keyCode == 65) {
-		player.aleft = false;
+		state.player.aleft = false;
 	    } else if (event.keyCode == 38 || event.keyCode == 87) {
-                player.aup = false;
+                state.player.aup = false;
 	    } else if (event.keyCode == 39 || event.keyCode == 68) {
-		player.aright = false;
+		state.player.aright = false;
 	    } else if (event.keyCode == 40 || event.keyCode == 83) {
-		player.adown = false;
+		state.player.adown = false;
 	    }
 	});
 
-
         var heartbeat = function() {
-            console.log("Thunk");
+            //console.log("Thunk");
             setTimeout(heartbeat, 2000);
         };
-        heartbeat();
+        //heartbeat();
 
     }
 })(typeof exports === 'undefined'? this['whiplash'] = {}: exports);
